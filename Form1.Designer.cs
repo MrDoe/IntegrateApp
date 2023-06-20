7f8ea9a5fc9d66f4597d66c4fc9c453803738e89
Christoph Döllinger <christoph.doellinger@med.uni-heidelberg.de>
2023-06-20 13:11:10 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace IntegrateApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCode = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlCode
+            // 
+            this.pnlCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCode.Location = new System.Drawing.Point(5, 5);
+            this.pnlCode.Name = "pnlCode";
+            this.pnlCode.Size = new System.Drawing.Size(790, 440);
+            this.pnlCode.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlCode);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace IntegrateApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pnlCode.Size = new Size(Width, Height);
+            pnlCode.Location = new Point(0, 0);
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        const int GWL_STYLE = -16;
+        const int WS_SYSMENU = 0x80000;
+        const int WS_CAPTION = 0xC00000;
+        const uint SWP_NOMOVE = 0x0002;
+        const uint SWP_NOSIZE = 0x0001;
+        const int WS_SIZEBOX = 0x00040000;
+
+        private Process p;
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            // open notepad.exe maximized in pnlCode
+            p = Process.Start("notepad.exe");
+            p.WaitForInputIdle();
+            SetParent(p.MainWindowHandle, pnlCode.Handle);
+            MoveWindow(p.MainWindowHandle, 0, 0, pnlCode.Width, pnlCode.Height, true);
+
+            // remove buttons in window title
+            int style = GetWindowLong(p.MainWindowHandle, GWL_STYLE);
+            style = style & ~WS_SYSMENU & ~WS_CAPTION;
+            SetWindowLong(p.MainWindowHandle, GWL_STYLE, style);
+
+            // set fixed borders for notepad.exe
+            pnlCode.BorderStyle = BorderStyle.FixedSingle;
+
+            // disable resizing of notepad.exe window
+            SetWindowLong(p.MainWindowHandle, GWL_STYLE, GetWindowLong(p.MainWindowHandle, GWL_STYLE) & ~WS_SIZEBOX);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pnlCode.Size = new Size(Width, Height);
+
+            // resize notepad.exe
+            MoveWindow(p.MainWindowHandle, 0, 0, pnlCode.Width, pnlCode.Height, true);
+        }
+    }
+}
